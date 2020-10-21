85df0a54462356efb3df65298fb6df39d813ba67
Tocolmas <44924466+Tocolmas@users.noreply.github.com>
2020-10-21 14:07:23 +0200

    Add files via upload

---
+++ exo2.1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    File *fichier = NULL;
+    char texte[200];
+    char filename[1024+1];
+
+    fichier = fopen(filename.txt,w);
+
+    printf ("quel texte voulez vous introduire dans le fichier\n");
+    fgets (texte, 200, stdin);
+
+    printf ("quel est le nom du fichier dans lequel il faut écrire\n");
+    fgets (filename, 1024, stdin);
+
+    echo "texte" > filemame.txt
+    wc filename.txt
+
+
